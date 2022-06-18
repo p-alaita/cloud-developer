236c63c0e89a48578d36757dd514f6e6eda42c31
Patience  Alaita <emene.patience@gmail.com>
2022-06-18 09:00:34 +0100

    Third commit

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   if(!image_url) {
     return res.status(400).send('requires an image url');
   }
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+
 
   try {
     const filteredpath= await filterImageFromURL(image_url);
@@ -35,12 +39,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     return res.status(400).send(' Hey There! looks like image_url is not filtered')
   }
 });
-
-  
-
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
